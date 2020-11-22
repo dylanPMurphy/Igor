a9b1d646e39931863b6842456b721a83c02483d2
MRichards2nd <elkabong4th@gmail.com>
2020-11-22 09:56:59 -0800

    urls and views nav routes added

--- igor/igor_app/urls.py
+++ igor/igor_app/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
+    url(r'^$', views.profile),
+    path(r'logout', include('login_reg.urls')),
+    path('about/', views.about),
+    path('questions', views.questions),
 ]
--- igor/igor_app/views.py
+++ igor/igor_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Welcome to Igor')
+def profile(request):
+    #profile page
+    return render(request, 'pages/profile.html')
+
+def about(request):
+    #directs to About Igor
+    return render(request, 'pages/about.html')
+
+def questions(request):
+    #directs to general Q&A board
+    return render(request, 'pages/wanted.html')
