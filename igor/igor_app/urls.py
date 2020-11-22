9a5880f6ab3d18fe00e8f167fdb70fdbe1e96570
MRichards2nd <elkabong4th@gmail.com>
2020-11-22 11:55:26 -0800

    updated paths and views imports

--- igor/igor_app/urls.py
+++ igor/igor_app/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.profile),
-    path(r'logout', include('login_reg.urls')),
     path('about/', views.about),
     path('questions', views.questions),
 ]
--- igor/igor_app/views.py
+++ igor/igor_app/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from login_reg.models import User
+from .models import *
 
 # Create your views here.
 def profile(request):
-    #profile page
-    return render(request, 'pages/profile.html')
+    user = User.objects.get(id=request.session['userid'])
+    context = {
+        'user':user
+    }
+    return render(request, 'pages/profile.html', context)
 
 def about(request):
-    #directs to About Igor
     return render(request, 'pages/about.html')
 
 def questions(request):
-    #directs to general Q&A board
     return render(request, 'pages/wanted.html')
